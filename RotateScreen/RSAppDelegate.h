bb84ed975bd7f263d7c2b06f75286e0aa86feb27
Hiromichi Yamada <yamada@torques.jp>
2014-07-19 14:29:38 +0900

    Initial Commit

---
+++ RotateScreen/RSAppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  RSAppDelegate.h
+//  RotateScreen
+//
+//  Created by 山田宏道 on 2014/07/19.
+//  Copyright (c) 2014年 Torques Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface RSAppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
---
+++ RotateScreen/main.m
@@ -0,0 +1,18 @@
+//
+//  main.m
+//  RotateScreen
+//
+//  Created by 山田宏道 on 2014/07/19.
+//  Copyright (c) 2014年 Torques Inc. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+#import "RSAppDelegate.h"
+
+int main(int argc, char * argv[])
+{
+	@autoreleasepool {
+	    return UIApplicationMain(argc, argv, nil, NSStringFromClass([RSAppDelegate class]));
+	}
+}
